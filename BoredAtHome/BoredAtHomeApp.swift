1471420b2e01546e88c4792c7a81c31d360bf72a
Jake Tamayo <hellojtmatthew@icloud.com>
2022-04-30 17:32:11 -0500

    Initial Commit

---
+++ BoredAtHome/BoredAtHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredAtHomeApp.swift
+//  BoredAtHome
+//
+//  Created by Jacob Tamayo on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BoredAtHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
